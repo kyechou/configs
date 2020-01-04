205c8c890f7d4c206e3d85290db0d2b2d77b3e78
kyechou <kuanyenchou@gmail.com>
2020-01-04 14:09:33 -0600

    Fix polybar scroll order

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 if command -v xrandr >/dev/null 2>&1; then
-	for m in $(xrandr --query | grep '\<connected\>' | cut -d ' ' -f 1); do
-		MONITOR="$m" polybar --reload top >/dev/null &
-	done
+    for m in $(xrandr --query | grep '\<connected\>' | cut -d ' ' -f 1); do
+        MONITOR="$m" polybar --reload top >/dev/null &
+    done
 fi
