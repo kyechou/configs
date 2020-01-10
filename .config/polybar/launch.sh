9bf5c2b900b6adb2f64d4af5eff03c4d24d195ed
kyechou <kuanyenchou@gmail.com>
2020-01-10 16:21:22 -0600

    Fix the polybar bspwm issue

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,7 +8,17 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 0.5; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 if command -v xrandr >/dev/null 2>&1; then
-    for m in $(xrandr --query | grep '\<connected\>' | cut -d ' ' -f 1); do
+    for m in $(xrandr --listmonitors | sed '1d' | awk -F ' ' '{print $4}'); do
         MONITOR="$m" polybar --reload top >/dev/null &
+    done
+fi
+
+# trigger an update on each non-empty desktop for polybar display
+if command -v bspc >/dev/null 2>&1; then
+    sleep 1
+    for desktop in $(bspc query -D); do
+        if bspc query -N -d $desktop; then
+            bspc config -d $desktop top_padding 0
+        fi
     done
 fi
