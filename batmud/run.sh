4fecfef8bac37214e93d0dd7d3dedf24164cd97e
kyechou <kuanyenchou@gmail.com>
2020-03-02 23:41:35 -0600

    Update config

--- batmud/run.sh
+++ batmud/run.sh
@@ -15,11 +15,11 @@ touch logs/{stats,public,private,social,trade}
 if ! tmux has-session -t batmud 2>/dev/null; then
     tmux new-session -d -s batmud -n batmud -x- -y- 'tt++ -t BatMUD rc.tin'
     tmux split-window -t batmud:batmud.0 -h -l  60 'watch -ctn 0.5 "cat map.out"'
-    tmux split-window -t batmud:batmud.0 -bh -l 60 'tail -f logs/public'
+    tmux split-window -t batmud:batmud.0 -bh -l 60 'tail -n 200 -f logs/public'
     tmux split-window -t batmud:batmud.0 -bv -l 13 'watch -ctn 0.5 "cat logs/stats"'
-    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -f logs/private'
-    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -f logs/social'
-    tmux split-window -t batmud:batmud.5 -v -l  15 'tail -f logs/trade'
+    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -n 200 -f logs/private'
+    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -n 200 -f logs/social'
+    tmux split-window -t batmud:batmud.5 -v -l  15 'tail -n 200 -f logs/trade'
     tmux select-pane -t batmud:batmud.4
 fi
 tmux attach-session -t batmud
