81e7aef436f796b6b33a7ad18f37f7cf4c2609e7
kyechou <kuanyenchou@gmail.com>
2020-03-04 15:46:36 -0600

    Update map and config

--- batmud/run.sh
+++ batmud/run.sh
@@ -10,16 +10,17 @@ cd "$SCRIPT_DIR"
     (echo '[!] Please run this game without root privilege' >&2; exit 1)
 
 mkdir -p logs
-touch logs/{stats,public,private,social,trade}
+touch logs/{stats,entity-stats,public,private,social,trade}
 
 if ! tmux has-session -t batmud 2>/dev/null; then
     tmux new-session -d -s batmud -n batmud -x- -y- 'tt++ -t BatMUD rc.tin'
     tmux split-window -t batmud:batmud.0 -h -l  60 'watch -ctn 0.5 "cat map.out"'
     tmux split-window -t batmud:batmud.0 -bh -l 60 'tail -n 200 -f logs/public'
-    tmux split-window -t batmud:batmud.0 -bv -l 12 'watch -ctn 0.5 "cat logs/stats"'
-    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -n 200 -f logs/private'
-    tmux split-window -t batmud:batmud.1 -v -l  10 'tail -n 200 -f logs/social'
-    tmux split-window -t batmud:batmud.5 -v -l  15 'tail -n 200 -f logs/trade'
-    tmux select-pane -t batmud:batmud.4
+    tmux split-window -t batmud:batmud.0 -bv -l 11 'watch -ctn 0.5 "cat logs/stats"'
+    tmux split-window -t batmud:batmud.0 -h -l  30 'watch -ctn 0.5 "cat logs/entity-stats"'
+    tmux split-window -t batmud:batmud.2 -v -l  12 'tail -n 200 -f logs/private'
+    tmux split-window -t batmud:batmud.2 -v -l  12 'tail -n 200 -f logs/social'
+    tmux split-window -t batmud:batmud.6 -v -l  15 'tail -n 200 -f logs/trade'
+    tmux select-pane -t batmud:batmud.5
 fi
 tmux attach-session -t batmud
