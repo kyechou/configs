10b46a0268e6d6738ced74abe78e8446652fbaad
kyechou <kuanyenchou@gmail.com>
2020-10-13 12:51:00 -0500

    Remove batmud scripts

--- manage.sh
+++ manage.sh
@@ -67,7 +67,7 @@ NAME='unknown'
 }
 
 [[ $DEPLOY_ALL -eq 1 ]] && { \
-    $RSYNC .Xmodmap batmud switch-monitor.sh ~/
+    $RSYNC .Xmodmap switch-monitor.sh ~/
     $RSYNC .config/bspwm .config/sxhkd .config/cmus .config/dunst \
         .config/polybar .config/termite .config/zathura .config/vlc \
         .config/newsboat ~/.config/
@@ -84,7 +84,7 @@ NAME='unknown'
 
 [[ $SYNC -eq 1 ]] && { \
     $RSYNC ~/.bashrc ~/.bash_profile ~/.profile ~/.vimrc ~/.vim ~/.gitconfig \
-        ~/.tmux.conf ~/.Xmodmap ~/batmud ~/switch-monitor.sh ./
+        ~/.tmux.conf ~/.Xmodmap ~/switch-monitor.sh ./
     $RSYNC ~/.config/bspwm ~/.config/sxhkd ~/.config/cmus ~/.config/dunst \
         ~/.config/polybar ~/.config/ranger ~/.config/termite ~/.config/zathura \
         ~/.config/vlc ~/.config/newsboat .config/
