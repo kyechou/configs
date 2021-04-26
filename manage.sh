03b86ef84b73a671381f08df6f4ed97e7067e26d
kyechou <kuanyenchou@gmail.com>
2021-04-26 11:47:36 -0500

    Disable syncing zathurarc

--- manage.sh
+++ manage.sh
@@ -71,9 +71,8 @@ NAME='unknown'
 [[ $DEPLOY_ALL -eq 1 ]] && { \
     $RSYNC .Xmodmap switch-gpu.sh ~/
     $RSYNC .config/bspwm .config/sxhkd .config/cmus .config/dunst \
-        .config/polybar .config/termite .config/zathura .config/vlc \
-        .config/newsboat .config/redshift \
-        ~/.config/
+        .config/polybar .config/termite .config/vlc .config/newsboat \
+        .config/redshift ~/.config/
     mkdir -p ~/.java/.userPrefs/org && \
         $RSYNC .java/.userPrefs/org/jabref ~/.java/.userPrefs/org/
     mkdir -p ~/.workrave && $RSYNC .workrave/workrave.ini ~/.workrave/
@@ -92,8 +91,7 @@ NAME='unknown'
         ~/.tmux.conf ~/.Xmodmap ~/switch-gpu.sh ./
     $RSYNC ~/.config/yay ~/.config/nnn ~/.config/bspwm ~/.config/sxhkd \
         ~/.config/cmus ~/.config/dunst ~/.config/polybar ~/.config/termite \
-        ~/.config/zathura ~/.config/vlc ~/.config/newsboat ~/.config/redshift \
-        .config/
+        ~/.config/vlc ~/.config/newsboat ~/.config/redshift .config/
     mkdir -p .java/.userPrefs/org && \
         $RSYNC ~/.java/.userPrefs/org/jabref .java/.userPrefs/org/
     mkdir -p .workrave && $RSYNC ~/.workrave/workrave.ini .workrave/
