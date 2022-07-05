0a77bdc26527391ed8916ce5ce4d356827e221b0
kyechou <kuanyenchou@gmail.com>
2022-07-05 16:10:19 -0500

    Make the parameters proportional to the display size

--- .config/wallpaper.sh
+++ .config/wallpaper.sh
@@ -5,9 +5,30 @@ set -euo pipefail
 wallpaper="/tmp/wallpaper.png"
 width="$(xrandr | head -n1 | cut -d ' ' -f 8 | tr -d ,)"
 display_height="$(xrandr | head -n1 | cut -d ' ' -f 10 | tr -d ,)"
-#bar_height="$(grep ^height $HOME/.config/polybar/config.ini | cut -d ' ' -f 3)"
-#height="$((display_height - bar_height))"
-height=$display_height
+bar_height="$(grep ^height $HOME/.config/polybar/config.ini | cut -d ' ' -f 3)"
+height="$((display_height - bar_height))"
+dot_radius_percentage=0.0028
+dot_border_width_percentage=0.00138
+toplists_font_percentage=0.014
+toplists_col_padding_percentage=0.008
+toplists_row_height_percentage=0.0208
+tree_font_percentage=0.0105
+dot_radius=$(echo "$dot_radius_percentage * $height" | bc)
+dot_border_width=$(echo "$dot_border_width_percentage * $height" | bc)
+toplists_font_size=$(echo "$toplists_font_percentage * $height" | bc)
+toplists_col_padding=$(echo "$toplists_col_padding_percentage * $height" | bc)
+toplists_row_height=$(echo "$toplists_row_height_percentage * $height" | bc)
+tree_font_size=$(echo "$tree_font_percentage * $height" | bc)
+cpulist_x_percentage=0.260 # 0.238
+cpulist_y_percentage=-0.080 # 0
+memlist_x_percentage=0.260 # 0.385
+memlist_y_percentage=0.080 # 0
+tree_radius_percentage=0.059
+cpulist_x=$(echo "$cpulist_x_percentage * $width" | bc)
+cpulist_y=$(echo "$cpulist_y_percentage * $height" | bc)
+memlist_x=$(echo "$memlist_x_percentage * $width" | bc)
+memlist_y=$(echo "$memlist_y_percentage * $height" | bc)
+tree_radius_increment=$(echo "$tree_radius_percentage * $width" | bc)
 
 source "$HOME/.config/colorscheme.sh"
 fg_color=${fujiWhite:1}
@@ -21,8 +42,10 @@ link_color=(${waveBlue1:1} ${autumnRed:1}cc)
 pscircle \
     --background-color=$bg_color \
     --collapse-threads=1 \
-    --dot-radius=4 \
-    --dot-border-width=2 \
+    --cpulist-center="${cpulist_x}:${cpulist_y}" \
+    --memlist-center="${memlist_x}:${memlist_y}" \
+    --dot-radius=$dot_radius \
+    --dot-border-width=$dot_border_width \
     --dot-border-color-max=${dot_color[1]} \
     --dot-border-color-min=${dot_color[0]} \
     --dot-color-max=${dot_color[1]} \
@@ -35,17 +58,17 @@ pscircle \
     --toplists-bar-color=$bar_fg \
     --toplists-bar-height=7 \
     --toplists-bar-width=70 \
-    --toplists-column-padding=12 \
+    --toplists-column-padding=$toplists_col_padding \
     --toplists-font-color=$fg_color \
     --toplists-font-face="Iosevka Etoile" \
-    --toplists-font-size=18 \
+    --toplists-font-size=$toplists_font_size \
     --toplists-pid-font-color=$pid_font_color \
-    --toplists-row-height=30 \
+    --toplists-row-height=$toplists_row_height \
     --tree-font-color=$fg_color \
     --tree-font-face="Iosevka" \
-    --tree-font-size=16 \
-    --tree-radius-increment=200 \
+    --tree-font-size=$tree_font_size \
+    --tree-radius-increment=$tree_radius_increment \
     --output-height=$height \
     --output-width=$width \
     --output="$wallpaper"
-feh --bg-fill "$wallpaper"
+feh --bg-max "$wallpaper" --image-bg "#$bg_color"
