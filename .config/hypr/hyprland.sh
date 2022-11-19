fb4207eb9c9d8c52a50db30b047b9c7d7617b736
kyechou <kuanyenchou@gmail.com>
2022-11-18 20:06:45 -0600

    Update hyprland configs

--- .config/hypr/hyprland.sh
+++ .config/hypr/hyprland.sh
@@ -11,7 +11,6 @@ export GDK_BACKEND='wayland,x11'
 export QT_AUTO_SCREEN_SCALE_FACTOR=1
 export QT_QPA_PLATFORM='wayland;xcb'
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
-export QT_QPA_PLATFORMTHEME=qt5ct
 
 # Nvidia
 # https://wiki.hyprland.org/Configuring/Environment-variables/#nvidia-specific
@@ -26,7 +25,9 @@ if [[ "$dGPU_status" = connected ]]; then
     export GBM_BACKEND=nvidia-drm
     export __GLX_VENDOR_LIBRARY_NAME=nvidia
     export __GLX_GSYNC_ALLOWED=1
-    export __GL_VRR_ALLOWED=0
+    export __GL_VRR_ALLOWED=1
+    export __GL_GSYNC_ALLOWED=1
+    export WLR_DRM_NO_ATOMIC=1
     export WLR_NO_HARDWARE_CURSORS=1
     export WLR_DRM_DEVICES=/dev/dri/card0
 fi
