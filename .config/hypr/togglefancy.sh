b2bb902486c6e95a68faef1e649cadab9b488609
kyechou <kuanyenchou@gmail.com>
2023-07-04 00:53:40 -0500

    Update hyprland configs and add a script for toggling effects

---
+++ .config/hypr/togglefancy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+animation_enabled=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+
+if [[ "$animation_enabled" == 1 ]]; then
+    hyprctl --batch "\
+        keyword general:border_size 1;\
+        keyword decoration:rounding 0;\
+        keyword decoration:active_opacity 1.0;\
+        keyword decoration:inactive_opacity 1.0;\
+        keyword decoration:fullscreen_opacity 1.0;\
+        keyword decoration:blur 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword animations:enabled 0;\
+    " >/dev/null
+else
+    hyprctl reload
+fi
