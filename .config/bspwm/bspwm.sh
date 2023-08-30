dbf0d2825fb158fc60f922c902f33eb78174ceb5
kyechou <kuanyenchou@gmail.com>
2023-08-30 17:29:35 -0500

    Remove nvidia related code for launching bspwm

--- .config/bspwm/bspwm.sh
+++ .config/bspwm/bspwm.sh
@@ -1,34 +1,31 @@
 #!/bin/bash
 
-NVFILE="/etc/X11/xorg.conf.d/20-nvidia.conf"
-BACKUP="$NVFILE.back"
+# # Nvidia
+# # https://wiki.hyprland.org/Configuring/Environment-variables/#nvidia-specific
+# mapfile -t dGPU_status < <(
+#     for p in /sys/class/drm/*/status; do
+#         con=${p%/status}
+#         echo -n "${con#*/card?-}:"
+#         cat "$p"
+#     done | grep -E '\<DP-|\<HDMI-' | cut -d: -f2
+# )
+#
+# dGPU_connected=0
+# for s in "${dGPU_status[@]}"; do
+#     if [[ "$s" = connected ]]; then
+#         dGPU_connected=1
+#     fi
+# done
+#
+# if [[ $dGPU_connected -ne 0 ]]; then
+#     if [[ -f "$BACKUP" ]] && [[ ! -f "$NVFILE" ]]; then
+#         sudo mv "$BACKUP" "$NVFILE"
+#     fi
+# else
+#     if [[ -f "$NVFILE" ]] && [[ ! -f "$BACKUP" ]]; then
+#         sudo mv "$NVFILE" "$BACKUP"
+#     fi
+# fi
 
-# Nvidia
-# https://wiki.hyprland.org/Configuring/Environment-variables/#nvidia-specific
-mapfile -t dGPU_status < <(
-    for p in /sys/class/drm/*/status; do
-        con=${p%/status}
-        echo -n "${con#*/card?-}:"
-        cat "$p"
-    done | grep -E '\<DP-|\<HDMI-' | cut -d: -f2
-)
-
-dGPU_connected=0
-for s in "${dGPU_status[@]}"; do
-    if [[ "$s" = connected ]]; then
-        dGPU_connected=1
-    fi
-done
-
-if [[ $dGPU_connected -ne 0 ]]; then
-    if [[ -f "$BACKUP" ]] && [[ ! -f "$NVFILE" ]]; then
-        sudo mv "$BACKUP" "$NVFILE"
-    fi
-else
-    if [[ -f "$NVFILE" ]] && [[ ! -f "$BACKUP" ]]; then
-        sudo mv "$NVFILE" "$BACKUP"
-    fi
-fi
-
-exec startx > "$HOME/.local/share/xorg/client.log" 2>&1
+exec startx >"$HOME/.local/share/xorg/client.log" 2>&1
 
