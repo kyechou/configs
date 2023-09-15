0713d4a1712426b70b2a8bd2b1698038483f3243
kyechou <kuanyenchou@gmail.com>
2023-09-15 09:19:46 -0500

    Update hyprland config

--- .config/hypr/visuals.sh
+++ .config/hypr/visuals.sh
@@ -3,16 +3,7 @@
 animation_enabled=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
 
 if [[ "$animation_enabled" == 1 ]]; then
-    hyprctl --batch "\
-        keyword general:border_size 1;\
-        keyword decoration:rounding 0;\
-        keyword decoration:active_opacity 1.0;\
-        keyword decoration:inactive_opacity 1.0;\
-        keyword decoration:fullscreen_opacity 1.0;\
-        keyword decoration:blur:enabled 0;\
-        keyword decoration:drop_shadow 0;\
-        keyword animations:enabled 0;\
-    " >/dev/null
+    hyprctl keyword animations:enabled 0 >/dev/null
 else
-    hyprctl reload >/dev/null
+    hyprctl keyword animations:enabled 1 >/dev/null
 fi
