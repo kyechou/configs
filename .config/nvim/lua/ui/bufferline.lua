9ccbe13c3a8e1f8ac7a53d1ed688e7c83d512eee
kyechou <kuanyenchou@gmail.com>
2024-02-13 23:02:54 -0600

    Update bufferline style

--- .config/nvim/lua/ui/bufferline.lua
+++ .config/nvim/lua/ui/bufferline.lua
@@ -10,9 +10,9 @@ return {
         local bufferline = require('bufferline')
         bufferline.setup({
             options = {
-                style_preset = bufferline.style_preset.no_italic,
-                mode = 'tabs', -- 'buffers' | 'tabs'
-                themable = true, -- allows highlight groups to be overriden
+                style_preset = bufferline.style_preset.minimal,
+                mode = 'tabs',    -- 'buffers' | 'tabs'
+                themable = true,  -- allows highlight groups to be overriden
                 numbers = 'none', -- 'none' | 'ordinal' | 'buffer_id' | 'both'
                 indicator = {
                     -- icon = '▎', -- this should be omitted if indicator style is not 'icon'
@@ -24,8 +24,8 @@ return {
                 left_trunc_marker = '',
                 right_trunc_marker = '',
                 max_name_length = 18,
-                max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-                truncate_names = true, -- whether or not tab names should be truncated
+                max_prefix_length = 15,   -- prefix used when a buffer is de-duplicated
+                truncate_names = true,    -- whether or not tab names should be truncated
                 tab_size = 1,
                 diagnostics = 'nvim_lsp', -- false | 'nvim_lsp' | 'coc'
                 diagnostics_update_in_insert = false,
@@ -35,7 +35,7 @@ return {
                         text = function() return vim.fn.getcwd() end,
                         highlight = 'Directory',
                         text_align = 'left', -- 'left' | 'center' | 'right'
-                        separator = true, -- true | '?'
+                        separator = true,    -- true | '?'
                     }
                 },
                 color_icons = true, -- whether or not to add the filetype icon highlights
@@ -44,8 +44,8 @@ return {
                 show_close_icon = false,
                 show_tab_indicators = false,
                 show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
-                persist_buffer_sort = true, -- whether custom sorted buffers should persist
-                move_wraps_at_ends = false, -- whether the move command "wraps" at the first or last position
+                persist_buffer_sort = true,   -- whether custom sorted buffers should persist
+                move_wraps_at_ends = false,   -- whether the move command "wraps" at the first or last position
                 separator_style = { '', '' }, -- 'slant' | 'slope' | 'thick' | 'thin' | { 'any', 'any' }
                 enforce_regular_tabs = false,
                 always_show_bufferline = true,
